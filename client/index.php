faabd5998370fa1ad1c0e2357a89ce27c9d6871e
ParadoxCalm <66328573+paradoxcalm@users.noreply.github.com>
2025-09-17 11:23:04 +0300

    Add base components styles to client modal

--- client/index.php
+++ client/index.php
@@ -16,6 +16,7 @@ if (empty($_SESSION['user_id'])) {
     <title>IDEAL TranSport - Клиентская панель</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../styles/components.css">
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/request-modal.css">
 </head>
